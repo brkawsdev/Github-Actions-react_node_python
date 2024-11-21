073e76879b40961ac5d74ae48b78abb023d9ff0b
harsha <selviharsha@gmail.com>
2024-11-21 16:41:30 +0530

    Flake 8

--- modules/03.Python_Flask_Pipeline/test_app.py
+++ modules/03.Python_Flask_Pipeline/test_app.py
@@ -1,11 +1,15 @@
 import pytest
+
+
 from app import app
+
 
 @pytest.fixture
 def client():
     # Create a test client using Flask's test client
     with app.test_client() as client:
         yield client
+
 
 def test_home_route(client):
     # Test the home route
