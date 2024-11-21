ae9f9014419a250849e497565bcbd1882a647c20
harsha <selviharsha@gmail.com>
2024-11-21 16:44:04 +0530

    Linting fixed

--- modules/03.Python_Flask_Pipeline/app.py
+++ modules/03.Python_Flask_Pipeline/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app)
 
@@ -8,6 +9,7 @@ CORS(app)
 @app.route('/')
 def home():
     return jsonify(message="Hello, World!")
+
 
 @app.route('/api/message', methods=['GET'])
 def get_message():
